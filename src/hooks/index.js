a537be939fd06f43a49b7873b1c708ebc82eece0
윤동우 <osydooo@gmail.com>
2020-03-20 17:06:34 +0900

    useLoginCheck

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,12 +1,12 @@
-export { useLoading, useDefaultData, useDefaultPeopleData } from "./useRoot";
-export { useProjectLoading, useProjectData } from "./useProject";
-export { usePeopleLoading, usePeopleData } from "./usePeople";
+export { useLoading, useDefaultData, useDefaultPeopleData } from './useRoot';
+export { useProjectLoading, useProjectData } from './useProject';
+export { usePeopleLoading, usePeopleData } from './usePeople';
 export {
   useProjectDetailLoading,
-  useProjectDetailData
-} from "./useProjectDetail";
-export { usePeopleDetailLoading, usePeopleDetailData } from "./usePeopleDetail";
-export { useMakeProjectData, useMakeProjectLoading } from "./useMakeProject";
-export { useMakeProfileData, useMakeProfileLoading } from "./useMakeProfile";
-export { useLoginData, useLoginLoading } from "./useLogin";
-export { useRegisterData, useRegisterLoading } from "./useRegister";
+  useProjectDetailData,
+} from './useProjectDetail';
+export { usePeopleDetailLoading, usePeopleDetailData } from './usePeopleDetail';
+export { useMakeProjectData, useMakeProjectLoading } from './useMakeProject';
+export { useMakeProfileData, useMakeProfileLoading } from './useMakeProfile';
+export { useLoginData, useLoginLoading, useLoginCheck } from './useLogin';
+export { useRegisterData, useRegisterLoading } from './useRegister';
--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,12 +1,13 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 export function useLoginLoading() {
   const dispatch = useDispatch();
   const { isLoading, isError } = useSelector(state => state.Login);
   const [loadState, setLoadState] = useState({
     open: false,
-    text: "로딩 중..."
+    text: '로딩 중...',
   }); // 메시지 상태메시지
 
   useEffect(() => {
@@ -25,9 +26,23 @@ export function useLoginLoading() {
 
 export const useLoginData = () => {
   const [loginState, setLoginState] = useState({
-    id: "",
-    password: ""
+    id: '',
+    password: '',
   });
 
   return [{ loginState }, setLoginState];
+};
+
+export const useLoginCheck = () => {
+  const location = useLocation();
+  const token = window.sessionStorage.getItem('accessToken');
+
+  useEffect(() => {
+    if (token === null) {
+      window.location.replace('/auth/login');
+      sessionStorage.setItem('redirectUrl', location.pathname);
+    }
+  }, []);
+
+  return token;
 };
