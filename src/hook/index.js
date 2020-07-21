82d9fe43ba00fed6fd4f810261910da9f2a90741
inho2736 <limcara@nate.com>
2020-07-21 23:06:03 +0900

    fix: merge 오류 해결
    상세설명
    - hooks/index.js 수정
    
    Resolves: #해결된 이슈넘버
    See also: #관계된 이슈

--- src/hook/index.js
+++ src/hook/index.js
@@ -1,7 +1,13 @@
 export { useTemporaryApi } from "./useHook";
 export { useRequest } from "./useRequest";
 export { default as useMove } from "./usePageMove";
-export { useProjectListState, useProjectSaveEffect } from "./project";
 export { usePeopleListState, usePeopleSaveEffect } from "./people";
 export { useRegisterEffect } from "./auth/useRegister";
-export { useProjectListState, useProjectSaveEffect, useProjectDetailState, useProjectDetailEffect, useProjectRecruitEffect, useProjectApplyEffect } from './project';
+export {
+  useProjectListState,
+  useProjectSaveEffect,
+  useProjectDetailState,
+  useProjectDetailEffect,
+  useProjectRecruitEffect,
+  useProjectApplyEffect
+} from "./project";
