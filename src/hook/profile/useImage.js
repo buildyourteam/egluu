4c000756e6fa2eaa7cade87af0894f2044884642
inho2736 <limcara@nate.com>
2020-07-24 16:00:14 +0900

    refactor: profile 에 이미지 지정 안해서 나는 error 감지해, 기본 이미지 띄우기
    상세설명
    - error.response.data 를 사용
    
    Resolves:
    See also: #21

--- src/hook/profile/useImage.js
+++ src/hook/profile/useImage.js
@@ -27,8 +27,16 @@ export const useImage = (
   useEffect(() => {
     if (rejected) {
       if (error) {
-        alert(error);
-        console.log(error);
+        //alert(error);
+        console.log(error.response.data);
+        // 이미지 설정한적 없어서 get 이미지가 에러날 때 기본이미지로 대체
+        if (error.response.data.error === 305) {
+          setImgState({
+            ...imgState,
+            imgUrl:
+              "https://i.pinimg.com/236x/21/88/fd/2188fd41b8d31930acc43b7b197e6dfd.jpg"
+          });
+        }
       }
     }
   }, [rejected]);
