bf0d11e0cf6c8da5c736b7e5470ac6ae193cb366
dongwoo <osydooo@gmail.com>
2020-08-15 18:30:05 +0900

    refactor: project box update dday 적용 및 버그 수정 project box url 적용

--- src/components/Project/ProjectBox.js
+++ src/components/Project/ProjectBox.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   Card,
   CardImg,
@@ -12,62 +13,86 @@ import {
 } from "reactstrap";
 import sampleimg from "../icon/baseImg.png";
 import "./Project.css";
+const moment = require("moment-timezone"); //상단에 선언
 
 export default function ProjectBox(props) {
   const staticProjectData = props.data;
+  const nowDay = Date.parse(moment().tz("Asia/Seoul").format());
+  const dday = Math.floor(
+    (Date.parse(staticProjectData.endDate) - nowDay) / (24 * 3600 * 1000)
+  );
   // 0으로 나눌 경우 예외처리!
   const developerPercent =
-    (staticProjectData.currentMember.developer /
-      staticProjectData.needMember.developer) *
-    100;
+    staticProjectData.currentMember.developer !== 0
+      ? (staticProjectData.currentMember.developer /
+          staticProjectData.needMember.developer) *
+        100
+      : 0;
   const plannerPercent =
-    (staticProjectData.currentMember.planner /
-      staticProjectData.needMember.planner) *
-    100;
+    staticProjectData.currentMember.planner !== 0
+      ? (staticProjectData.currentMember.planner /
+          staticProjectData.needMember.planner) *
+        100
+      : 0;
+
   const designerPercent =
-    (staticProjectData.currentMember.designer /
-      staticProjectData.needMember.designer) *
-    100;
+    staticProjectData.currentMember.designer !== 0
+      ? (staticProjectData.currentMember.designer /
+          staticProjectData.needMember.designer) *
+        100
+      : 0;
+
   const etcPercent =
-    (staticProjectData.currentMember.etc / staticProjectData.needMember.etc) *
-    100;
+    staticProjectData.currentMember.etc !== 0
+      ? (staticProjectData.currentMember.etc /
+          staticProjectData.needMember.etc) *
+        100
+      : 0;
+
   return (
     <div id="ProjectBoxCard">
-      <Card>
-        <CardImg
-          width="100%"
-          height="150"
-          src={`https://egluuapi.codingnome.dev/projects/image/${staticProjectData.projectId}`}
-          alt="Card image cap"
-        />
-        <CardBody>
-          <CardTitle>{staticProjectData.projectName}</CardTitle>
-          <CardSubtitle>{staticProjectData.teamName}</CardSubtitle>
-          <CardText>D-{staticProjectData.dday}</CardText>
-          <Row xs="12">
-            <Col xs="6">
-              <Progress color="success" value={developerPercent}>
-                {developerPercent}
-              </Progress>
-              <CardText>developer</CardText>
-              <Progress color="success" value={plannerPercent}>
-                {plannerPercent}
-              </Progress>
-              <CardText>planner </CardText>
-            </Col>
-            <Col xs="6">
-              <Progress color="success" value={designerPercent}>
-                {designerPercent}
-              </Progress>
-              <CardText>designer</CardText>
-              <Progress color="success" value={etcPercent}>
-                {etcPercent}
-              </Progress>
-              <CardText>etc</CardText>
-            </Col>
-          </Row>
-        </CardBody>
-      </Card>
+      <Link
+        to={{
+          pathname: `${props.url}`,
+        }}
+        style={{ textDecoration: "none" }}
+      >
+        <Card>
+          <CardImg
+            width="240px"
+            height="180px"
+            src={`https://egluuapi.codingnome.dev/projects/image/${staticProjectData.projectId}`}
+            alt="Card image cap"
+          />
+          <CardBody>
+            <CardTitle>{staticProjectData.projectName}</CardTitle>
+            <CardSubtitle>{staticProjectData.teamName}</CardSubtitle>
+            <CardText>D-{dday}</CardText>
+            <Row xs="12">
+              <Col xs="6">
+                <Progress color="success" value={developerPercent}>
+                  {developerPercent}
+                </Progress>
+                <CardText>developer</CardText>
+                <Progress color="success" value={plannerPercent}>
+                  {plannerPercent}
+                </Progress>
+                <CardText>planner </CardText>
+              </Col>
+              <Col xs="6">
+                <Progress color="success" value={designerPercent}>
+                  {designerPercent}
+                </Progress>
+                <CardText>designer</CardText>
+                <Progress color="success" value={etcPercent}>
+                  {etcPercent}
+                </Progress>
+                <CardText>etc</CardText>
+              </Col>
+            </Row>
+          </CardBody>
+        </Card>
+      </Link>
     </div>
   );
 }
