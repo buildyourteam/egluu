1eb6409b7ab8d60465366d6194d2e5f80fb20b00
inho2736 <limcara@nate.com>
2020-08-29 17:03:01 +0900

    refactor: people card design 수정 상세설명
    Resolves: #해결된 이슈넘버
    See also: #62

--- src/components/People/PeopleBox.js
+++ src/components/People/PeopleBox.js
@@ -10,50 +10,68 @@ import {
   Row,
   Button
 } from "reactstrap";
-import sampleimg from "../icon/baseImg.png";
+import { makeStyles } from "@material-ui/core/styles";
+import { Badge as Stack } from "reactstrap";
+import Badge from "@material-ui/core/Badge";
 import Level from "./Level";
 import "./People.css";
 import { Link } from "react-router-dom";
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    "& > *": {
+      marginBottom: theme.spacing(2)
+    },
+    "& .MuiBadge-badge": {
+      height: "30px",
+      width: "30px",
+      fontSize: "1rem",
+      borderRadius: "15px",
+      color: "#ffffff",
+      backgroundColor: "#007bff"
+    }
+  }
+}));
 
 export default function PeopleBox(props) {
+  const classes = useStyles();
   const data = props.data;
 
   return (
-    <div id="PeopleBoxCard">
+    <div id="PeopleBoxCard" className={classes.card}>
       <Link
         to={{
           pathname: `${props.url}`
         }}
-        style={{ textDecoration: "none" }}
+        style={{ textDecoration: "none", color: "#000000" }}
       >
-        <Card>
-          <CardImg
-            top
-            width="100%"
-            src={`${process.env.REACT_APP_BASE_URL}profile/image/${data.userId}`}
-            alt="Card image cap"
-          />
-          <CardBody>
-            <div>
-              <Row xs="12">
-                <Col xs="8">
-                  {/* <CardTitle>{data.userName}</CardTitle> */}
-                  <CardSubtitle>{data.userId}</CardSubtitle>
-                </Col>
-                <Col xs="4">
-                  <Level data={data.grade} />
-                </Col>
-              </Row>
-            </div>
+        <Badge badgeContent={String(data.grade)} className={classes.root}>
+          <Card>
+            <CardImg
+              top
+              width="100%"
+              src={`${process.env.REACT_APP_BASE_URL}profile/image/${data.userId}`}
+              alt="Card image cap"
+            />
+            <CardBody>
+              <div id="card-title">
+                <CardTitle>{data.userId}</CardTitle>
+              </div>
 
-            <CardText id="card-text">
-              {data.stack === null && " "}
-              {data.stacks.map(value => {
-                return "#" + value + " ";
-              })}
-            </CardText>
-          </CardBody>
-        </Card>
+              <CardText id="card-text">
+                {data.stack === null && " "}
+                {data.stacks.map(value => {
+                  return (
+                    <Stack color="secondary" pill>
+                      # {value}{" "}
+                    </Stack>
+                  );
+                })}
+              </CardText>
+            </CardBody>
+          </Card>
+        </Badge>
       </Link>
     </div>
   );
