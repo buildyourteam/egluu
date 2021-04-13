2c190300542600f2f64246634600c8abab29e8b3
akjfal <osydooo@gmail.com>
2021-04-13 23:31:52 +0900

    Updates

---
+++ precache-manifest.40feddd185ec557cf274594d9c81e873.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1043625d0e39cdedc84ec4461c664e12",
+    "url": "/egluu/index.html"
+  },
+  {
+    "revision": "3aa760f44053501c88d2",
+    "url": "/egluu/static/css/2.cab98286.chunk.css"
+  },
+  {
+    "revision": "373a7022e0515fa97161",
+    "url": "/egluu/static/css/main.0cf9a934.chunk.css"
+  },
+  {
+    "revision": "3aa760f44053501c88d2",
+    "url": "/egluu/static/js/2.7997216a.chunk.js"
+  },
+  {
+    "revision": "915326de05c592dc1e8b7670c85db1b9",
+    "url": "/egluu/static/js/2.7997216a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "373a7022e0515fa97161",
+    "url": "/egluu/static/js/main.47673bcc.chunk.js"
+  },
+  {
+    "revision": "d1e5635da2b03476457d",
+    "url": "/egluu/static/js/runtime-main.fd3febab.js"
+  },
+  {
+    "revision": "c40f4ed989b72e9464a13e92e6886086",
+    "url": "/egluu/static/media/baseImg.c40f4ed9.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4259f1a7270eb6aa8e1111208c39bd9a.js"
+  "/egluu/precache-manifest.40feddd185ec557cf274594d9c81e873.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/egluu/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
